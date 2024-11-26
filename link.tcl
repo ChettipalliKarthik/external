9f3e56c5f0a43b96e4c4203c06d691cabd40aa9d
ChettipalliKarthik <chettipallikarthik150@gmail.com>
2024-11-26 22:56:32 +0530

    third commit

--- link.tcl
+++ link.tcl
@@ -1,30 +1,53 @@
-# Initialize the simulator
 set ns [new Simulator]
 
-# Enable Link State routing protocol
 $ns rtproto LS
 
-# Create nodes
 set node1 [$ns node]
+set node2 [$ns node]
+set node3 [$ns node]
+set node4 [$ns node]
+set node5 [$ns node]
+set node6 [$ns node]
+set node7 [$ns node]
 
-# Trace files
 set tf [open out.tr w]
 $ns trace-all $tf
+
 set nf [open out.nam w]
 $ns namtrace-all $nf
 
-# Assign labels and colors to nodes
 $node1 label "node 1"
+$node2 label "node 2"
+$node3 label "node 3"
+$node4 label "node 4"
+$node5 label "node 5"
+$node6 label "node 6"
+$node7 label "node 7"
 
 $node1 label-color blue
+$node2 label-color red
+$node3 label-color red
+$node4 label-color blue
+$node5 label-color blue
+$node6 label-color blue
+$node7 label-color blue
 
-# Create duplex links between nodes
 $ns duplex-link $node1 $node2 1.5Mb 10ms DropTail
+$ns duplex-link $node2 $node3 1.5Mb 10ms DropTail
+$ns duplex-link $node3 $node4 1.5Mb 10ms DropTail
+$ns duplex-link $node4 $node5 1.5Mb 10ms DropTail
+$ns duplex-link $node5 $node6 1.5Mb 10ms DropTail
+$ns duplex-link $node6 $node7 1.5Mb 10ms DropTail
+$ns duplex-link $node7 $node1 1.5Mb 10ms DropTail
 
-# Define link orientations
 $ns duplex-link-op $node1 $node2 orient left-down
+$ns duplex-link-op $node2 $node3 orient left-down
+$ns duplex-link-op $node3 $node4 orient right-down
+$ns duplex-link-op $node4 $node5 orient right
+$ns duplex-link-op $node5 $node6 orient right-up
+$ns duplex-link-op $node6 $node7 orient left-up
+$ns duplex-link-op $node7 $node1 orient left-up
 
-# Define TCP connection
 set tcp2 [new Agent/TCP]
 $ns attach-agent $node1 $tcp2
 
@@ -33,26 +56,22 @@ $ns attach-agent $node4 $sink2
 
 $ns connect $tcp2 $sink2
 
-# Attach FTP application to TCP
 set traffic_ftp2 [new Application/FTP]
 $traffic_ftp2 attach-agent $tcp2
 
-# Finish procedure
 proc finish {} {
-    global ns nf tf
+    global ns nf
     $ns flush-trace
     close $nf
-    close $tf
     exec nam out.nam &
     exit 0
 }
 
-# Schedule events
-$ns at 0.5 "$traffic_ftp2 start"
+$ns at 0.5 "traffic_ftp2 start"
 $ns rtmodel-at 1.0 down $node2 $node3
 $ns rtmodel-at 2.0 up $node2 $node3
-$ns at 3.0 "$traffic_ftp2 stop"
+$ns at 3.0 "traffic_ftp2 start"
+$ns at 4.0 "traffic_ftp2 stop"
 $ns at 5.0 "finish"
 
-# Run the simulation
 $ns run
